30ddafafa8eaa9707d2749a519fe34d95a910581
Muhammad Haroon <m_haroon96@hotmail.com>
2023-11-29 19:20:28 +0100

    added code

--- docker/receiver.py
+++ docker/receiver.py
@@ -3,6 +3,9 @@ import socket
 
 PACKET_SIZE = 1024
 SEQ_ID_SIZE = 4
+MESSAGE_SIZE = PACKET_SIZE - SEQ_ID_SIZE
+EXPECTED_SEQ_ID = 0
+RECEIVED_DATA = {}
 
 def create_acknowledgement(seq_id):
     return int.to_bytes(seq_id, SEQ_ID_SIZE, signed=True, byteorder='big') + b'ack'
@@ -13,10 +16,6 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as udp_socket:
     # bind to 0.0.0.0 so external 
     udp_socket.bind(("0.0.0.0", 5001))
 
-    # file to write to
-    # write to some file on the hard disk
-    recv = open('/hdd/recv.txt', 'wb')
-
     print("Receiver running")
     # start receiving packets
     while True:
@@ -24,6 +23,9 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as udp_socket:
         try:
             # receive the packet
             packet, client = udp_socket.recvfrom(PACKET_SIZE)
+            
+            if random.random() < 0.1:
+                continue
 
             # get the message id
             seq_id, message = packet[:SEQ_ID_SIZE], packet[SEQ_ID_SIZE:]
@@ -31,21 +33,34 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as udp_socket:
             # if the message id is -1, we have received all the packets
             seq_id = int.from_bytes(seq_id, signed=True, byteorder='big')
             
-            if seq_id == -1:
-                # send the acknowledgement
-                udp_socket.sendto(create_acknowledgement(seq_id), client)
-                break
+            print('received', seq_id)
             
-            # write message to file
-            recv.seek(seq_id)
-            recv.write(message)
-
+            # keep track of received sequences
+            RECEIVED_DATA[seq_id] = message
+            
+            # check if sequence id is same as expected and move forward
+            if seq_id <= EXPECTED_SEQ_ID and len(RECEIVED_DATA[seq_id]) > 0:
+                while EXPECTED_SEQ_ID in RECEIVED_DATA:
+                    EXPECTED_SEQ_ID += len(RECEIVED_DATA[seq_id])
+            
+            # create ack id
+            ack_id = EXPECTED_SEQ_ID
+            
+            
+            print('sending', ack_id)
+            
             # create the acknowledgement
-            acknowledgement = create_acknowledgement(seq_id)
+            acknowledgement = create_acknowledgement(ack_id)
 
             # send the acknowledgement
             udp_socket.sendto(acknowledgement, client)
+            
+            # check if all data received (empty message)
+            if len(message) == 0 and ack_id == seq_id:
+                break
         except socket.timeout:
             timeouts += 1
 
-    recv.close()
+with open('recv.txt', 'wb') as f:
+    for sid in sorted(RECEIVED_DATA.keys()):
+        f.write(RECEIVED_DATA[sid])
---
+++ docker/sender.py
@@ -0,0 +1,85 @@
+import socket
+
+# total packet size
+PACKET_SIZE = 1024
+# bytes reserved for sequence id
+SEQ_ID_SIZE = 4
+# bytes available for message
+MESSAGE_SIZE = PACKET_SIZE - SEQ_ID_SIZE
+# total packets to send
+WINDOW_SIZE = 5
+ACKS = {}
+
+# read data
+with open('send.txt', 'rb') as f:
+    data = f.read()
+ 
+# create a udp socket
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as udp_socket:
+
+    # bind the socket to a OS port
+    udp_socket.bind(("localhost", 5000))
+    udp_socket.settimeout(1)
+    
+    # start sending data from 0th sequence
+    seq_id = 0
+    while seq_id < len(data):
+        
+        # create messages
+        messages = []
+        acks = {}
+        seq_id_tmp = seq_id
+        for i in range(WINDOW_SIZE):
+            # construct messages
+            # sequence id of length SEQ_ID_SIZE + message of remaining PACKET_SIZE - SEQ_ID_SIZE bytes
+            message_data = data[seq_id_tmp : seq_id_tmp + MESSAGE_SIZE]
+            message = int.to_bytes(seq_id_tmp, SEQ_ID_SIZE, byteorder='big', signed=True) + message_data
+            messages.append((seq_id_tmp, message))
+            # check if not last message
+            if len(message_data) == 0:
+                break
+            # create ack
+            acks[seq_id_tmp] = False
+            # move seq_id tmp pointer ahead
+            seq_id_tmp += len(message_data)
+
+        # send messages
+        for sid, message in messages:
+            print('sending', sid)
+            udp_socket.sendto(message, ('localhost', 5001))
+        
+        # wait for acknowledgement
+        while True:
+            try:
+                # wait for ack
+                ack, _ = udp_socket.recvfrom(PACKET_SIZE)
+                
+                # extract ack id
+                ack_id = int.from_bytes(ack[:SEQ_ID_SIZE], byteorder='big')
+                
+                # update acks below cumulative ack
+                for _id in acks:
+                    if _id < ack_id:
+                        acks[_id] = True
+                        
+                ACKS[ack_id] = ACKS.get(ack_id, 0) + 1
+                
+                # all acks received, move on
+                if all(acks.values()):
+                    break
+            except socket.timeout:
+                # no ack received, resend unacked messages
+                for sid, message in messages:
+                    if not acks[sid]:
+                        print('sending', sid)
+                        udp_socket.sendto(message, ('localhost', 5001))
+                
+        # move sequence id forward
+        seq_id = seq_id_tmp
+        
+    # send final closing message
+    print('here')
+    udp_socket.sendto(int.to_bytes(seq_id, 4, signed=True, byteorder='big'), ('localhost', 5001))
+    
+    
+print(ACKS)
